ea95ff37da3940277e626d5e45327b0a79147ef7
satoruegawa-dotcom <satoru.egawa@givery.co.jp>
2026-06-10 14:14:01 +0900

    Add files via upload

---
+++ middleware.js
@@ -0,0 +1,23 @@
+export function middleware(req) {
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASSWORD) {
+      return;
+    }
+  }
+
+  return new Response('Authentication Required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  });
+}
+
+export const config = {
+  matcher: '/:path*',
+};
